482e679e797ed06acef5ee68fb6c2b4ed0e1c784
Gordan Sajević <gsajevic@hotmail.com>
2014-11-20 19:13:09 +0100

    piramida

---
+++ Piramida3.java
@@ -0,0 +1,26 @@
+package lab9;
+
+import java.util.Scanner;
+
+public class Piramida3 {
+
+	public static void main(String[] args) {
+		Scanner unos = new Scanner (System.in);
+		int broj = unos.nextInt();
+		for (int i=1; i<=broj; i++)
+		{
+			for (int j=1; j<=broj-i; j++)
+			{
+				System.out.printf("  ");
+			}
+		
+			for (int k=1; k<=i; k++)
+			{
+				System.out.printf("%3d ", k);
+			}
+			System.out.println("");
+		}
+
+	}
+
+}
