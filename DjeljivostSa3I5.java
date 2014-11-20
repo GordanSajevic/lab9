6e2adeac36bf1a6e33a5734c58ccb6222a44e2f5
Gordan Sajević <gsajevic@hotmail.com>
2014-11-20 13:19:12 +0100

    djeljivost

---
+++ DjeljivostSa3I5.java
@@ -0,0 +1,28 @@
+package lab9;
+
+import java.util.Scanner;
+
+public class DjeljivostSa3I5 {
+
+	public static void main(String[] args) {
+		Scanner unos = new Scanner(System.in);
+		System.out.println("Unesite broj: ");
+		double broj = unos.nextInt();
+		int sa3=0;
+		int sa5=0;
+		for (int i=1; i<=broj; i++)
+		{
+			if (i%3==0)
+			{
+				sa3++;
+			}
+			if (i%5==0)
+			{
+				sa5++;
+			}
+		}
+		System.out.println("Brojevi djeljivi sa 3: " + sa3);
+		System.out.println("Brojevi djeljivi sa 5: " + sa5);
+	}
+
+}
